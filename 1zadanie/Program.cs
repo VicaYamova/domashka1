f0e3506a120ced3a5c68dd40b96604aeedcc91ec
Vika <vica_che@mail.ru>
2022-08-30 23:45:13 +0300

    1zadanie

---
+++ 1zadanie/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите пятизначное число:   ");
+string? number = Console.ReadLine();
+
+if(number[0]==number[4]&&number[1]==number[3])
+{
+    Console.WriteLine($"Число: {number} - палиндром");
+}
+
+else
+{
+    Console.WriteLine($"Число: {number} - НЕ палиндром");
+}
